b0f60c7baab6afdd4c734c685844b4f79c253267
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-09 23:39:06 -0700

    sessions save

--- server.js
+++ server.js
@@ -11,7 +11,22 @@ const path = require('path');
 // Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
+// for express-sessions library - store user cookies
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
