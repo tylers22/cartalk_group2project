ab8649583f3976bdd3c6ee012b5175bacb570619
Nicholas Dixon <whitelotus32@Nicholass-MBP.attlocal.net>
2021-11-09 22:05:11 -0800

    Finished login submission

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,20 +1,26 @@
 /* Login submission */
-function signupFormHandler(event) {
+async function loginFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
   
-    if (username && email && password) {
-      fetch('/api/users', {
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
-          username,
           email,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
-      }).then((response) => {console.log(response)})
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
     }
   }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
