f569ab8439f2b4a352d9ec6f41118632b9a8f71e
Nicholas Dixon <whitelotus32@Nicholass-MBP.attlocal.net>
2021-11-09 19:44:18 -0800

    home-routes fix

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,24 +1,6 @@
 /* This file will contain all of the user-facing 
 routes, such as the homepage and login page 
-<<<<<<< HEAD
 Still needs url */
-=======
-Added the url - test to make sure it works*/
-router.get('/', (req, res) => {
-    res.render('homepage', {
-      id: 1,
-      post_url: 'https://cartalk2021.herokuapp.com/',
-      title: 'Toyota RAV4 Review',
-      created_at: new Date(),
-      vote_count: 10,
-      comments: [{}, {}],
-      user: {
-        username: 'whitelotus32'
-      }
-    });
-  });
-
->>>>>>> develop
 const router = require('express').Router();
 
 // router.get('/', (req, res) => {
