1137e2b0852a31e20ae88fee05ad0d7833b28d99
Nicholas Dixon <whitelotus32@Nicholass-MBP.attlocal.net>
2021-11-10 20:36:00 -0800

    Added single post route

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -36,5 +36,21 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-module.exports = router;
+/* Route renders single post forum */
+router.get('/post/:id', (req, res) => {
+  const post = {
+    id: 1,
+    post_url: 'https://handlebarsjs.com/guide/',
+    title: 'Handlebars Docs',
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: 'test_user'
+    }
+  };
 
+  res.render('single-post', { post });
+});
+
+module.exports = router;
