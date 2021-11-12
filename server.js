c80b68c83fafe56ae55d3310f25aa9e800f03794
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-12 11:36:31 -0700

    server.js doubled const of hbs deleted, server now working

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ app.use(session(sess));
 
 const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
-
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
