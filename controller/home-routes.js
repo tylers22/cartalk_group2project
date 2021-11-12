b49b74378fb7ebcc2da9f698575550cbeb59e489
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-11 20:53:34 -0700

    reverting code to previous updates

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,51 +1,37 @@
+
 /* This file will contain all of the user-facing 
 routes, such as the homepage and login page 
 Still needs url */
 const router = require('express').Router();
+//importing the necessary modules and models
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Post, User, Comment } = require('../models');
 
-// get all posts for homepage
+// router.get('/', (req, res) => {
+//   res.render('homepage');
+// });
+
+/* move the review to a different page and restructure homepage
+review disappeared. maybe put into post-routes? */
 router.get('/', (req, res) => {
-  console.log('======================');
-  Post.findAll({
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-
-      res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  console.log(req.session);
+  res.render('homepage', {
+    posts,
+    loggedIn: req.session.loggedIn
+  });
 });
 
-// get single post
+/* Route renders login */
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  
+  res.render('login');
+});
+
+/* Route renders single post forum */
 router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
@@ -53,7 +39,6 @@ router.get('/post/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
       'title',
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
@@ -79,9 +64,11 @@ router.get('/post/:id', (req, res) => {
         return;
       }
 
+      // serialize the data
       const post = dbPostData.get({ plain: true });
 
-      res.render('single-post', {
+      // pass data to template
+      res.render('single-post', { 
         post,
         loggedIn: req.session.loggedIn
       });
@@ -90,15 +77,6 @@ router.get('/post/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
-
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
 });
 
 module.exports = router;
