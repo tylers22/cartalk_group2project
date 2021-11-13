c652b2535f040449380b469fe3f9b76812d3ca77
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-12 22:16:36 -0700

    Jaws DB added

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,20 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = process.env.AWS_HOST
-    ? new Sequelize(process.env.AWS_DB_NAME, process.env.AWS_USER, process.env.AWS_PASSWORD, {
-        host: process.env.AWS_HOST,
+//Jaws DB connection
+const sequelize = //process.env.AWS_HOST ?
+     new Sequelize("vakp1om9uhkcguvx","goc49ffi9ayizlco","usg9dtr3kytl6lvv", {
+        host:"j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         dialect: 'mysql',
         port: 3306
     })
-    : new Sequelize('cartalk2021_db', 'root', '1998Hlsg8sql', {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
+    //:
+    //This is the local mysql db connection
+    //  new Sequelize('cartalk2021_db', 'root', '1998Hlsg8sql', {
+    //     host: 'localhost',
+    //     dialect: 'mysql',
+    //     port: 3306
+    // });
 
 // create connection to our database, pass in your MySQL information for username and password - Hibram: 1998Hlsg8sql
 // const sequelize = new Sequelize('cartalk2021_db', 'root', 'Bootcamp2021!', {
@@ -22,7 +25,8 @@ const sequelize = process.env.AWS_HOST
 //     port: 3306
 // });
 
-// //create connection to AWS , pass in your MySQL information for username and password
+//NOT WORKING!!!
+ // //create connection to AWS , pass in your MySQL information for username and password
 // const sequelize = new Sequelize('cartalk2021', 'admin', 'C8rtalk!', {
 //     host: 'http://cartalk2021.caw2jnsosmx5.us-east-2.rds.amazonaws.com/',
 //     dialect: 'mysql',
